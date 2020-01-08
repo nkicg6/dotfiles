2e58e4e02846e83af2c7bded6f8576afe8def0fb
Nicholas George <nicholas.george32@gmail.com>
2020-01-07 22:20:11 -0700

    added readme and minor changes

---
+++ synchronize_hard_drives.sh
@@ -0,0 +1,9 @@
+# script to sync two hard drives
+
+# personal to lab
+echo '[INFO] Synching personal to lab'
+#rsync -v -a -u /Volumes/NICK_HD/RESTREPOLAB/images/ /Volumes/LABONE/images/
+# New hard drive 2018-06-09
+# Exclude . files with --exclude=".*" 2018-08-16
+rsync -v -a -u --max-size=3.9G --exclude=".*" /Volumes/Nickdata/ /Volumes/LABTWO/backup_personal_hd/
+echo '[INFO] DONE!'
