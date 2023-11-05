ceb70d286531b5e4c9ea825332a217534e432342
Nicholas George <nicholas.george32@gmail.com>
2023-11-04 22:18:55 -0400

    fix tmux session script

--- bin/bin/tmux-fzf.sh
+++ bin/bin/tmux-fzf.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
 session=$(find $HOME $HOME/dev $HOME/dotfiles $HOME/personal $HOME/bioteam -type d -mindepth 1 -maxdepth 1 |fzf)
 session_name=$(basename "$session"|tr . _)
-if ! tmux has-session -t "$session_name" 2>/dev/null; then
+tmux has-session -t "$session_name" 2>/dev/null
+if [ $? != 0 ]; then
   tmux new-session -s "$session_name" -c "$session" -d 
 fi
-tmux switch-client -t "$session_name"
+if [ -n "$TMUX" ]; then
+  tmux switch-client -t "$session_name"
+else
+  tmux attach-session -t "$session_name"
+fi
